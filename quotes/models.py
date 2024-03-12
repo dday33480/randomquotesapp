287aeb9684e10e3dc6c17908b9609c7588e37c94
David D <daviddufau@hotmail.co.uk>
2024-03-12 09:19:17 +0100

    feat: creates interface for quotes model in admin

--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Quote(models.Model):
+    quote_text = models.CharField(max_length=900)
+    quote_author = models.CharField(max_length=50)
+    quote_year = models.IntegerField()
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self) -> str:
+        return self.quote_text
