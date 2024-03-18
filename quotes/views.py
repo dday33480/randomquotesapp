81dc9ce5507e99ff6e7064febb7768613a8b8865
David D <daviddufau@hotmail.co.uk>
2024-03-18 15:44:30 +0100

    chore: Comment code

--- quotes/views.py
+++ quotes/views.py
@@ -3,6 +3,7 @@ from .models import Quote
 
 import random
 
+""" Get count of objects in DB for index page """
 def index(request):
         quote_count = Quote.objects.order_by("pub_date").count()
         context = {
@@ -11,6 +12,7 @@ def index(request):
         return render(request, "quotes/index.html", context)
 
 
+""" Get count of objects in DB for details page """
 def detail(request):
         quote_count = Quote.objects.order_by("pub_date").count()
         context = {
@@ -19,6 +21,7 @@ def detail(request):
         return render(request, "quotes/detail.html", context)
 
 
+""" Get all objects from DB and make random choice to dispaly random quote on results page """
 def results(request):
         quote_list = list(Quote.objects.all())
         random_quote = random.choice(quote_list)
