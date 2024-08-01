88c729f3ee38fb01b7706e3b9b15e7e5b777672f
David D <daviddufau@hotmail.co.uk>
2024-08-01 14:21:48 +0200

    feat: Adds categories to Quotes in back office

--- quotes/models.py
+++ quotes/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
+class QuoteCategory(models.Model):
+    category = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.category
+
+
+
 class Quote(models.Model):
+    category = models.ForeignKey('QuoteCategory', on_delete=models.CASCADE, default=1)
     quote_text = models.CharField(max_length=900)
     quote_author = models.CharField(max_length=50)
     quote_year = models.IntegerField()
