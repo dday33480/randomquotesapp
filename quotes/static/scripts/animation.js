13eb47a5871be11510435dad95cae048f39ae7dd
David D <daviddufau@hotmail.co.uk>
2024-08-14 12:08:19 +0200

    feat: Add javascript animations

---
+++ quotes/static/scripts/animation.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('btn-animation');
+
+
+    button.addEventListener('mousemove', (event) => {
+        const rect = button.getBoundingClientRect();
+        const x = event.clientX - rect.left - rect.width / 2;
+        const y = event.clientY - rect.top - rect.height / 2;
+
+        const scaleFactor = 1 + Math.sqrt(x * x + y * y) / 100;
+        const maxScale = 2;    
+        const scale = Math.min(1 + scaleFactor, maxScale);
+
+        button.style.transform = `scale(${scale})`;
+    });
+
+    button.addEventListener('mouseleave', () => {
+        button.style.transform = 'scale(1)';
+    });
+});
+
---
+++ quotes/static/scripts/transition.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.container');
+
+    container.classList.add('loaded');
+});
