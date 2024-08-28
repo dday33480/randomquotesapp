367c8975eba4555335988cf4caf33640b3d3f538
David D <daviddufau@hotmail.co.uk>
2024-08-28 16:36:14 +0200

    build: Sets Debug to False for Prod

--- randomquotesapp/settings.py
+++ randomquotesapp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f#*qop3w&r4iok_b-*(^!3(km7&m2xke4wb%6$&x+*bzj+6kqw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
